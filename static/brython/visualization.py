c8952d473ce9f448c9ac90d41af8971bc454e803
Burgos <nandobfer@yahoo.com.br>
2022-08-24 11:58:39 -0300

    started visualization footer front

--- static/brython/visualization.py
+++ static/brython/visualization.py
@@ -1,6 +1,7 @@
 from browser import document, ajax, html, bind, window, alert
 
 jQuery = window.jQuery
+missing_data = []
 
 def _ajax(url, onComplete, method='GET', data={}):
     req = ajax.Ajax()
@@ -9,8 +10,14 @@ def _ajax(url, onComplete, method='GET', data={}):
     req.set_header('content-type', 'application/x-www-form-urlencoded')
     req.send(data)
     
+def buildFooter(data):
+    for item in data:
+        print(item)
+
+    
 def initialRender(req):
     data = eval(req.text)
+    buildFooter(data[7:])
     for item in data:
         row = f'<tr><td><input type="checkbox" name="checkbox" id="checkbox-{item[0]}">'
         item.pop(0)
@@ -20,5 +27,7 @@ def initialRender(req):
                 break
         row += '<td><img src="/static/images/seta-roxa.svg" alt="seta-roxa"></img></td></tr>'
         jQuery('tbody').append(row)
+        if data.index(item) == 6:
+            break
     
 _ajax('/get_table_data/', initialRender, method='GET')
